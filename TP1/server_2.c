993d7b7f73cdc27b7872050ac4abbf87049b5ed6
Kali <romainbasketteur92@gmail.com>
2021-10-06 03:28:31 -0400

    question 1.2

---
+++ TP1/server_2.c
@@ -0,0 +1,41 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <time.h>
+#include <signal.h>
+#include <sys/types.h>
+
+int running = 1;
+
+void stop_handler(int sig)
+{
+    printf("Numéro du signal: %d\n", sig);
+    //running = 0;
+}
+
+int main()
+{
+    struct sigaction s;
+
+    srand(time(NULL));
+
+    s.sa_handler = stop_handler;
+
+    sigaction(SIGINT, &s, NULL);
+
+    sigaction(SIGTERM, &s, NULL);
+
+    printf("Je suis le début de la boucle infinie.\n");
+
+    while (running)
+    {
+        printf("Pid enfant: %d\n", getpid());
+        printf("Pid parent: %d\n", getppid());
+        printf("Nombre aléa entre 0 et 99: %d\n", rand() % 100);
+        sleep(1);
+    }
+
+    printf("Je suis la fin de la boucle infinie.\n");
+
+    return EXIT_SUCCESS;
+}
